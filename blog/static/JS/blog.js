a97880f554b39bfe768e60ff6c7897fd84704c5d
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-04-28 00:57:01 +0900

    JS追加

---
+++ blog/static/JS/blog.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const mainNav = document.querySelector("#mainNav")
+
+//windowの幅が992px以上の場合
+if (window.innerWidth > 992) {
+  let previousTop = 0;
+
+  // スクロールイベント
+  window.addEventListener('scroll', (e) => {
+    //トップを取得
+    let currentTop = document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (currentTop < previousTop) {
+      // スクロールUP
+      if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
+        mainNav.classList.add("is-visible");
+        } else {
+          // スクロールTOP
+          mainNav.classList.remove("is-visible", "is-fixed");
+        }
+      } else if (currentTop > previousTop) {
+      // スクロールDOWN
+        mainNav.classList.remove("is-visible");
+
+      if (currentTop > mainNav.clientHeight && !mainNav.classList.contains('is-fixed')) {
+        mainNav.classList.add("is-fixed");
+      }
+    }
+    previousTop = currentTop;
+  });
+}
