cbebd4e8e2080e40d9855218a31c3c0460005c37
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-04-27 16:10:32 +0900

    CSS追加

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.utils import timezone
+from blog.models import Post
+from django.views.generic import (ListView)
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+
+    def get_queryset(self):
+        return Post.objects.all()
+        # 後でこちらに変更
+        # return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
