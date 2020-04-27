e6f36ab6a42653439f6f1951f27af8750c19c641
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-04-27 16:46:12 +0900

    アプリケーション拡張

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from blog import views
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.utils import timezone
 from blog.models import Post
-from django.views.generic import (ListView)
+from django.views.generic import (ListView, DetailView)
 
 class PostListView(ListView):
     model = Post
@@ -9,4 +9,8 @@ class PostListView(ListView):
     def get_queryset(self):
         return Post.objects.all()
         # 後でこちらに変更
-        # return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+        # return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
