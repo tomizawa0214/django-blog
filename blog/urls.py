9fc30db2e1f45a3de4c13cbacf0878166fe11311
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-04-28 11:48:40 +0900

    固定ページ追加

--- blog/urls.py
+++ blog/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('post/<int:pk>/comment/', views.post_comment, name='post_comment'),
     path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
     path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    path('about/', views.AboutView.as_view(), name='about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -77,3 +77,6 @@ def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
     return redirect('post_detail', pk=comment.post.pk)
+
+class AboutView(TemplateView):
+    template_name = 'page/about.html'
