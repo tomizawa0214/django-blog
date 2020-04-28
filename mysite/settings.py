d9590d6a4727f85cac0d6708ac8ea9c29e6e19c1
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-04-28 12:23:02 +0900

    Herokuデプロイ

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -155,3 +156,5 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 IMAGE_ROOT = os.path.join(BASE_DIR, 'images')
 IMAGE_URL = '/images/'
+
+django_heroku.settings(locals())
