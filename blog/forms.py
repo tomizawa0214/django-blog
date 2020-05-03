000b8e21d812295a75bd899d393c078e5f6cec18
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-05-03 11:52:14 +0900

    選択リストにエラーメッセージ追加

--- blog/forms.py
+++ blog/forms.py
@@ -5,8 +5,22 @@ class PostForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(PostForm, self).__init__(*args, **kwargs)
         # self.fields['title'].widget.attrs = {'placeholder': '必ずご入力ください。'}
+        self.fields['author'].required = False
+        self.fields['category'].required = False
         self.fields['title'].required = False
         self.fields['text'].required = False
+
+    def clean_author(self):
+        author = self.cleaned_data['author']
+        if author == None:
+            raise forms.ValidationError ('※投稿者をを選択してください。')
+        return author
+
+    def clean_category(self):
+        category = self.cleaned_data['category']
+        if category == None:
+            raise forms.ValidationError ('※カテゴリを選択してください。')
+        return category
 
     def clean_title(self):
         title = self.cleaned_data['title']
@@ -18,7 +32,7 @@ class PostForm(forms.ModelForm):
         text = self.cleaned_data['text']
         if len(text) == 0:
             raise forms.ValidationError ('※本文を入力してください。')
-        return title
+        return text
 
     class Meta:
         model = Post
