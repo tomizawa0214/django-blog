704f3a1c22f1b78ac5d4b3478212ca8cc06da86a
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-05-05 00:24:14 +0900

    セキュリティ強化追加

--- mysite/settings.py
+++ mysite/settings.py
@@ -140,10 +140,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 LOGIN_REDIRECT_URL = '/'
 
 SITE_ID = 1
